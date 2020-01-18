7ae51b5be0f76d5df7c30103b8cbfc75571d154d
kasia m <kasia.napieracz@gmailcom>
2020-01-18 12:54:06 +0100

    naprawa

--- test/test_views.py
+++ test/test_views.py
@@ -13,5 +13,5 @@ class FlaskrTestCase(unittest.TestCase):
         ','.join(SUPPORTED) in rv.data
 
     def test_msg_with_output(self):
-        rv = self.get('/?output=json')
+        rv = self.add.get('/?output=json')
         self.assertEquals('{ "imie":"Kasia", "mgs":Hello World!"}', rv.data)
